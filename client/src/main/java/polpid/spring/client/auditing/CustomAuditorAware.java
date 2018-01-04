58216b992ec9d2f4035c468445470f9e00a8a858
blusky10 <blusky0910@gmail.com>
2018-01-04 23:12:08 +0900

    security 추가

--- client/src/main/java/polpid/spring/client/auditing/CustomAuditorAware.java
+++ client/src/main/java/polpid/spring/client/auditing/CustomAuditorAware.java
@@ -1,21 +1,20 @@
 package polpid.spring.client.auditing;
 
 
+
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import polpid.spring.client.domain.Account;
 
 public class CustomAuditorAware implements AuditorAware<Account> {
+
     @Override
     public Account getCurrentAuditor() {
-        return null;
-    }
 
-//    @Override
-//    public Account getCurrentAuditor() {
-//
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        if (null == authentication || !authentication.isAuthenticated()) {
-//            return null;
-//        }
-//        return (Account) authentication.getPrincipal(); }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (null == authentication || !authentication.isAuthenticated()) {
+            return null;
+        }
+        return (Account) authentication.getPrincipal(); }
 }
